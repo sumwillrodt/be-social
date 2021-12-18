93bb8b51d10d7a2d967205df75d981c8bac48b18
swillrodt <sumnerwillrodt@gmail.com>
2021-12-18 09:35:40 -0800

    create user methods and routes

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,12 @@
-const router = require('express')
+// Integrate API routes into server
+const router = require('express').Router();
+// Import API routes from /api/index.js 
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).send('<h1>404 Error</h1>');
+});
+
+module.exports = router;
