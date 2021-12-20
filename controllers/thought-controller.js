5b469aa67b24095f90b261e9440bc1d18ea73cd9
swillrodt <sumnerwillrodt@gmail.com>
2021-12-19 22:26:32 -0800

    update thought routes

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,14 +1,15 @@
-const { Thought, User } = require('../models');
+const { Thought } = require('../models/Thought');
 
 const thoughtController = {
   // GET to get all thoughts
   getAllThoughts(req, res) {
     Thought.find({})
-      .populate({
+     .populate({
         path: 'reactions',
         select: '-__v'
       })
       .select('-__v')
+      .sort({ _id: -1})
       .then(dbThoughtData => res.json(dbThoughtData))
       .catch(err => {
         console.log(err);
